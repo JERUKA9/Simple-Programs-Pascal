bea513511d94b1f452443bab12d837c25b4eb1b3
JERUKA9 <jeruka9@gmail.com>
2019-05-11 13:11:18 +0200

    Add files via upload

---
+++ Calc.pas
@@ -0,0 +1,21 @@
+program Calc;
+
+var
+  n1:integer;
+  n2:integer;
+
+
+begin
+
+n1:=3;
+n2:=3;
+  writeln(n1+n2); // calc functions//
+  writeln(n1-n2);
+  writeln(n1*n2);
+  writeln(n1/n2:2:3);
+  writeln(n1 div n2);
+  writeln(n1 mod n2);
+  readln;
+
+end.
+
