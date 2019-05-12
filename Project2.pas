5eb9ff7dc1de3116c8dccedbc35cfcdf27c542d7
JERUKA9 <jeruka9@gmail.com>
2019-05-12 12:43:31 +0200

    Add files via upload

---
+++ Project2.pas
@@ -0,0 +1,18 @@
+program Project1;
+
+const
+  a = 4;
+
+var
+  i:integer;    //multiply and adding  numbers//
+  n:integer;
+
+begin
+  i:=4;
+  n:=4;
+  write(i+n*a);
+  writeln;
+  readln;
+
+end.
+
