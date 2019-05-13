879ec99057e6ab5fb70da6af99e69d338ba35899
JERUKA9 <jeruka9@gmail.com>
2019-05-13 13:17:39 +0200

    Add files via upload

---
+++ Password.pas
@@ -0,0 +1,13 @@
+program Password;
+
+var
+  password:string;
+
+
+begin
+   password := 'string';
+   if password = 'string' then  //simple password program to fix//
+   writeln('good');
+      readln;
+end.
+
