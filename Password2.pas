24297728d6942eea7da5997851ffa94cc26d0a00
JERUKA9 <jeruka9@gmail.com>
2019-07-02 16:35:40 +0200

    Add files via upload

---
+++ Password2.pas
@@ -0,0 +1,23 @@
+program Password2;
+
+var
+
+   p:string;
+   d:ansistring;
+
+begin
+
+      p:='12345';
+      d:='Password';
+
+      if p+d ='!234 Password' then
+
+      writeln('Password correct');
+
+      readln;
+
+
+
+
+end.
+
