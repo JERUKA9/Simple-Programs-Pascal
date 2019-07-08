f61f5dc693ad490e935b4aa9268327fa029c5b66
JERUKA9 <jeruka9@gmail.com>
2019-07-08 15:28:47 +0200

    Add files via upload

---
+++ Stars3.pas
@@ -0,0 +1,28 @@
+program Stars3;
+
+
+var
+
+i,j,n1,n2:integer;
+
+
+begin
+
+n1:=1;
+n2:=3;
+for i:=1 to n2 do
+for j:=1 to n1 do
+begin
+writeln('*');
+n1:=n1+2;
+
+read;
+
+
+end;
+
+
+
+
+end.
+
