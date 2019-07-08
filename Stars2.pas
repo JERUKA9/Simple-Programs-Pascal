b24795ec8a39c4f4347a44cf2fc2f67dcfef243f
JERUKA9 <jeruka9@gmail.com>
2019-07-08 14:20:20 +0200

    Add files via upload

---
+++ Stars2.pas
@@ -0,0 +1,27 @@
+program Stars2;
+
+
+var
+
+i,n,n2:integer;
+
+
+begin
+
+    i:=2;
+
+    n:=2;
+
+    n2:=i*n*2;
+
+    if   n2 =n*i*2 then
+
+    write(i*n*2,'*');
+
+    readln;
+
+
+
+
+end.
+
