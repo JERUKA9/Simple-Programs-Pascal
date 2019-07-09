3f573f866cd551c25ee5dbeb52ae894871ab331b
JERUKA9 <jeruka9@gmail.com>
2019-07-09 13:30:49 +0200

    Add files via upload

---
+++ Array1.pas
@@ -0,0 +1,18 @@
+program Array1;
+
+type
+   Numbers=array[1..30] of integer;
+   Numbers2= array[1..30] of integer;
+   Numbers3=array[1..30] of integer;
+   Numbers4=array[1..30] of integer;
+
+
+begin
+
+//TODO//
+
+
+
+
+end.
+
