ebdbad279427e410ebee6478e19c7221921bde86
JERUKA9 <jeruka9@gmail.com>
2019-07-10 12:58:09 +0200

    Add files via upload

---
+++ Array2.pas
@@ -0,0 +1,23 @@
+program Array2;
+
+var
+   Numbers:array[1..30] of integer;
+   Numbers2:array[1..30] of integer;
+   Numbers3:array[1..30] of integer;
+   Numbers4:array[1..30] of integer;
+
+
+begin
+
+  Numbers[1]:= 1;
+
+
+  readln;
+
+
+
+
+
+
+end.
+
