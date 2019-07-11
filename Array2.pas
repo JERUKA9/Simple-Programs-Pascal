e4db0651ea1ecd1fbad3c095cc31f53b45d080c7
JERUKA9 <jeruka9@gmail.com>
2019-07-11 10:15:02 +0200

    Add files via upload

--- Array2.pas
+++ Array2.pas
@@ -1,6 +1,7 @@
 program Array2;
 
 var
+   i,j,k,l:integer;
    Numbers:array[1..30] of integer;
    Numbers2:array[1..30] of integer;
    Numbers3:array[1..30] of integer;
@@ -9,7 +10,19 @@ var
 
 begin
 
-  Numbers[1]:= 1;
+  for i:=1 to Length (Numbers)  do
+
+      for j:=1 to Length (Numbers)  do
+
+          for k:=1 to Length (Numbers)  do
+
+              for l:=1 to Length (Numbers)  do
+
+
+      Numbers[i] :=1;
+
+  Readln(Numbers[i]);
+
 
 
   readln;
