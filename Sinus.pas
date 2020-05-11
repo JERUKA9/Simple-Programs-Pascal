0c648969119f88bd3ce64c4a070daa747ec50ab6
JERUKA9 <jeruka9@gmail.com>
2020-05-11 12:06:23 +0200

    Add files via upload

---
+++ Sinus.pas
@@ -0,0 +1,18 @@
+program Sinus;
+
+var
+
+  x:double;
+
+
+begin
+  writeln('x',' sin(x)',' cos(x)');
+
+  x:=0.0;
+        writeln(x,sin(x), cos(x));
+        x:=x+0.1;
+         writeln(x,sin(x), cos(x));
+
+
+end.
+
